6668a9b2f3d0fc0bf10f59d2029412fa9cf1269b
Christopher Buquicchio <buquicchioc@gmail.com>
2016-10-07 15:34:40 -0400

    Major cleaup
    * unify tab/spaces for vector files
    * fix code that was generating gcc warnings

--- tests/minunit.h
+++ tests/minunit.h
@@ -8,12 +8,14 @@
 
 #define mu_suite_start() char *message = NULL
 
-#define mu_assert(test, message) if (!(test)) { log_err(message); return message; }
+#define mu_assert(test, message) if (!(test)) { log_err(message);\
+    return message; }
+
 #define mu_run_test(test) debug("\n-----%s", " " #test); \
     message = test(); tests_run++; if (message) return message;
 
 #define RUN_TESTS(name) int main(int argc, char *argv[]) {\
-    argc = 1; \
+    (void)argc;\
     debug("----- RUNNING: %s", argv[0]);\
     printf("----\nRUNNING: %s\n", argv[0]);\
     char *result = name();\
